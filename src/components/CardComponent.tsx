0ad448bbea1f43bd8c86b648079b3b7acd853fd1
Juan Ibiapina <juanibiapina@gmail.com>
2023-04-11 23:56:06 +0200

    feat: Add input overlay to enter number of repetitions
    Code generated by ChatGPT 4. The UI is broken and the number of
    repetitions is not yet taken into account.

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -8,22 +8,63 @@ const CardComponent: React.FC<{
   cardId: string;
   performable: boolean;
 }> = ({ cardId, performable }) => {
-  const performCard = useStore((store) => store.actions.performCard);
+  const performCard = useStore((state) => state.actions.performCard);
   const card = useStore((state) => state.game.cardPack.cards[cardId]);
 
-  const onClick = () => {
-    if (performable) {
-      performCard(cardId);
+  const [performing, setPerforming] = React.useState(false);
+  const [repetitions, setRepetitions] = React.useState(1);
+
+  const startPerforming = () => {
+    if (performable && !performing) {
+      setPerforming(true);
+    }
+  };
+
+  const submit = () => {
+    performCard(cardId); // TODO: pass repetitions
+  };
+
+  const cancel = () => {
+    setPerforming(false);
+  };
+
+  const increaseValue = () => {
+    setRepetitions((prevValue) => Math.min(prevValue + 1, 10));
+  };
+
+  const decreaseValue = () => {
+    setRepetitions((prevValue) => Math.max(prevValue - 1, 1));
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseInt(event.target.value);
+    if (newValue >= 0 && newValue <= 10) {
+      setRepetitions(newValue);
     }
   };
 
   return (
-    <div className="card" onClick={onClick}>
+    <div className="card" onClick={startPerforming}>
       <div className="title">{card.name}</div>
       <div className="illustration">
         <img src={card.image} alt={card.name} />
       </div>
       <div className="description">{card.description}</div>
+      {performing && (
+        <div className="performing-actions">
+          <button onClick={decreaseValue}>-</button>
+          <input
+            type="number"
+            value={repetitions}
+            onChange={handleInputChange}
+            min="1"
+            max="10"
+          />
+          <button onClick={increaseValue}>+</button>
+          <button onClick={submit}>Submit</button>
+          <button onClick={cancel}>Cancel</button>
+        </div>
+      )}
     </div>
   );
 };
